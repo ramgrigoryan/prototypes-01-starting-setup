88b7af7197dba20a5f957e102ee1209f82244bab
Vahram Grigoryan <ramgrigoryan@gmail.com>
2022-03-10 21:28:32 +0400

    Concepts are learned

--- prototypes-01-starting-setup/app.js
+++ prototypes-01-starting-setup/app.js
@@ -10,9 +10,22 @@
 function Person(age) {
   this.name = "Vahram";
   this.age = age;
-  this.greet = function(){
+  this.greet = function () {
     console.log(`Hello, I am ${this.name} and I am ${this.age} years old.`);
-  }
+  };
 }
+
+function FairyTale() {
+  this.story = () => {
+    console.log(`  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, id unde mollitia blanditiis illum dolores sed cupiditate enim officiis, harum numquam nihil? Mollitia consequuntur eos nobis, sunt nam ratione modi officia, deleniti quo ipsa fugit facilis commodi ut! Repudiandae, ut.
+        `);
+  };
+}
+Person.prototype.story = () => {
+  console.log(`  Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime, id unde mollitia blanditiis illum dolores sed cupiditate enim officiis, harum numquam nihil? Mollitia consequuntur eos nobis, sunt nam ratione modi officia, deleniti quo ipsa fugit facilis commodi ut! Repudiandae, ut.
+    `);
+};
+
 const me = new Person(25);
 me.greet();
+me.story();
